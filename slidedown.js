9541ad029fa82287650ddb92f1d3a2ce91d4c116
Jesse Zhong <jessewzhong@gmail.com>
2019-01-26 00:43:57 -0500

    slide arrow

--- slidedown.js
+++ slidedown.js
@@ -31,13 +31,16 @@ setTimeout(getHeight, 2000);
 
 function slide(elem) {
     var list = document.getElementsByClassName("slider");
+    var list2 = document.getElementsByClassName("slidebutton");
     var newList = Array.prototype.slice.call(list);
     var temp = newList.indexOf(elem);
     var nextList = document.getElementsByClassName("slide");
     if (nextList[temp].getBoundingClientRect().height === 0) {
         nextList[temp].style.height= heights[temp] + "px";
+        setTimeout(function () {list2[temp].style.transform = "scaleY(-1)"}, 1500)
     }
     else {
         nextList[temp].style.height= "0px";
+        setTimeout(function () {list2[temp].style.transform = "none"}, 1500)
     }
 }
