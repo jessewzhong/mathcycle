e945e413c1bda1a2ef89d01a30d374d31742da58
Jesse Zhong <jessewzhong@gmail.com>
2019-01-31 01:52:41 -0500

    js files

---
+++ maintainwidth.js
@@ -0,0 +1,11 @@
+function banana() {
+    var elem = document.getElementById("loginA");
+    var width = elem.getBoundingClientRect().width;
+    elem.style.width = width + "px";
+}
+
+function apple() {
+    var elem = document.getElementById("createloginA");
+    var width = elem.getBoundingClientRect().width;
+    elem.style.width = width + "px";
+}
