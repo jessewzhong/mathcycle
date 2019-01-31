0b68ff3b9dc6388054aa0eb999aa3aea32edf8e7
Jesse Zhong <jessewzhong@gmail.com>
2019-01-30 22:28:48 -0500

    more login stuff

--- display.js
+++ display.js
@@ -5,8 +5,10 @@ function display(elem) {
     var nextList = document.getElementsByClassName("hide");
     if (nextList[temp].style.opacity === "0" || nextList[temp].style.opacity === "") {
         nextList[temp].style.opacity = "1";
+        nextList[temp].style.cursor = "auto";
     }
     else {
         nextList[temp].style.opacity = "0";
+        nextList[temp].style.cursor = "default";
     }
 }
