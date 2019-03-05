3a488b8ff4b1a84dafb8b4a43ca16d53147ecbcf
Jesse Zhong <jessewzhong@gmail.com>
2019-03-04 22:06:00 -0500

    removed stuff

--- pictures/mathcycle/onethirdwidth.js
+++
@@ -1,6 +0,0 @@
-var list = document.getElementsByClassName("grid");
-for (var i = 0; i < list.length; i++){
-    var temp = list[i].getBoundingClientRect().width;
-    temp = 0.96 * temp / 3;
-    list[i].style.height = temp + "px";
-}
--- pictures/mathcycle/samewidth.js
+++
@@ -1,6 +0,0 @@
-var list = document.getElementsByClassName("grid2");
-for (var i = 0; i < list.length; i++){
-    var temp = list[i].getBoundingClientRect().width;
-    temp = 0.92 * temp;
-    list[i].style.height = temp + "px";
-}
